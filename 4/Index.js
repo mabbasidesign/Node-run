743bf7f656df9f63fe3f87133113e4d3a0c39615
unknown <mabbasidesign@yahoo.com>
2019-02-12 11:47:20 +0330

    add port

--- 4/Index.js
+++ 4/Index.js
@@ -9,4 +9,6 @@ app.get('/api/courses', (req, res) => {
   res.send([1, 2, 3]);
 })
 
-app.listen(3000, () => console.log('listening on port 3000'));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`listening on port ${port}...`));
