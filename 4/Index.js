030ac61cab7c5ec1e3994322521735cb49f8afcb
unknown <mabbasidesign@yahoo.com>
2019-02-12 11:20:38 +0330

    app.get

--- 4/Index.js
+++ 4/Index.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+
+app.get('/',  (req, res) => {
+  res.send('Heloo World')
 })
- 
-app.listen(3000)
+
+app.listen(3000, () => console.log('listening on port 3000'));
